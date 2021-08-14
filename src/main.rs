815f944a6b55377b5943efb475622e88eac8cc43
lovasoa <pere.jobs@gmail.com>
2021-08-14 02:03:40 +0200

    Split routes into independant functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+
 use serde::Serialize;
 use warp::{Filter, Reply};
 use warp::http::StatusCode;
@@ -11,25 +12,40 @@ mod application_state;
 #[tokio::main]
 async fn main() {
     let shared_pins_state = Arc::new(State::new());
-    let with_pins_state = warp::any().map(move || shared_pins_state.clone());
 
-    let gpio_hello = warp::path!("gpio")
-        .map(|| "This is the GPIO API");
 
-    let gpio_modify = warp::post()
-        .and(warp::path!("gpio" / String / u32))
-        .map(GpioPath::new)
-        .and(with_pins_state)
-        .and(warp::body::content_length_limit(10))
-        .and(warp::body::json())
-        .map(|gpio_path, state: Arc<State>, body| state.write(gpio_path, body))
-        .map(create_http_response);
-
-    let routes = gpio_hello.or(gpio_modify);
+    let routes =
+        gpio_get(shared_pins_state.clone())
+            .or(gpio_post(shared_pins_state.clone()));
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
+}
+
+type StateRef = Arc<State>;
+
+fn gpio_path(shared_pins_state: StateRef) -> impl Filter<Extract=(GpioPath, StateRef,), Error=warp::Rejection> + Clone {
+    warp::path!("gpio" / String / u32)
+        .map(GpioPath::new)
+        .and(warp::any().map(move || shared_pins_state.clone()))
+}
+
+
+fn gpio_post(state: StateRef) -> impl Filter<Extract=impl warp::Reply, Error=warp::Rejection> + Clone {
+    gpio_path(state)
+        .and(warp::post())
+        .and(warp::body::content_length_limit(10))
+        .and(warp::body::json())
+        .map(|gpio_path, state: Arc<State>, body| state.write(gpio_path, body))
+        .map(create_http_response)
+}
+
+fn gpio_get(state: StateRef) -> impl Filter<Extract=impl warp::Reply, Error=warp::Rejection> + Clone {
+    gpio_path(state)
+        .and(warp::get())
+        .map(|gpio_path, state: Arc<State>| state.read(gpio_path))
+        .map(create_http_response)
 }
 
 fn create_http_response<O: Serialize>(r: AppResult<O>) -> impl warp::Reply {
