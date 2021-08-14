844af74304e655af8ec98236a259fc10f8cce5f0
lovasoa <pere.jobs@gmail.com>
2021-08-14 23:11:36 +0200

    Add the ability to list devices and pins

--- src/application_state.rs
+++ src/application_state.rs
@@ -2,9 +2,10 @@ use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 use std::sync::{Arc, RwLock};
 
-use gpio_cdev::{Chip, LineHandle, LineRequestFlags};
 use gpio_cdev::errors::Error;
-use log::{debug, info};
+use gpio_cdev::{chips, Chip, LineDirection, LineHandle, LineInfo, LineRequestFlags};
+use log::{debug, error, info};
+use serde::Serialize;
 
 #[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Debug, Clone)]
 pub struct GpioPath {
@@ -20,7 +21,7 @@ impl GpioPath {
 
 #[derive(Debug)]
 pub enum AppError {
-    Gpio(gpio_cdev::errors::Error)
+    Gpio(gpio_cdev::errors::Error),
 }
 
 pub type AppResult<O> = Result<O, AppError>;
@@ -49,19 +50,26 @@ impl State {
     pub fn new() -> Self {
         let active_pins = HashMap::<GpioPath, Arc<LineHandle>>::new();
         Self {
-            pins: RwLock::new(active_pins)
+            pins: RwLock::new(active_pins),
         }
     }
-
 
     /// Given an action to perform on a pin, try to perform it on an already opened line handle, and
     /// if the action fails, retries it with a freshly opened handle.
     /// (this gracefully handles writing to a pin that was previously opened as input)
-    fn do_with_handle<F, O, E>(&self, gpio_path: GpioPath, flags: LineRequestFlags, action: F) -> AppResult<O>
-        where F: Fn(&LineHandle) -> Result<O, E>,
-              AppError: From<E> {
+    fn do_with_handle<F, O, E>(
+        &self,
+        gpio_path: GpioPath,
+        flags: LineRequestFlags,
+        action: F,
+    ) -> AppResult<O>
+    where
+        F: Fn(&LineHandle) -> Result<O, E>,
+        AppError: From<E>,
+    {
         debug!("Trying to acquire a read lock on pins");
-        { // Read lock
+        {
+            // Read lock
             let pins = self.pins.read().unwrap();
             if let Some(handle) = pins.get(&gpio_path) {
                 if let Ok(r) = action(handle) {
@@ -82,7 +90,8 @@ impl State {
         info!("Making an {:?} request", flags);
         let handle = line.request(flags, 0, "http-gpio")?;
         let arc_handle = Arc::new(handle);
-        { // Application state is locked
+        {
+            // Application state is locked
             let mut pins = self.pins.write().unwrap();
             debug!("Saving the pin handle for later");
             pins.insert(gpio_path, Arc::clone(&arc_handle));
@@ -97,6 +106,77 @@ impl State {
     }
 
     pub fn write(&self, gpio_path: GpioPath, value: u8) -> AppResult<()> {
-        self.do_with_handle(gpio_path, LineRequestFlags::OUTPUT, |line| line.set_value(value))
+        self.do_with_handle(gpio_path, LineRequestFlags::OUTPUT, |line| {
+            line.set_value(value)
+        })
+    }
+}
+
+pub fn list_chips() -> AppResult<Vec<GpioDeviceDescription>> {
+    Ok(chips()?
+        .enumerate()
+        .flat_map(|(num, c)| match c {
+            Ok(chip) => Some(GpioDeviceDescription::new(chip)),
+            Err(e) => {
+                error!("Unable to access chip {} in list: {}", num, e);
+                None
+            }
+        })
+        .collect())
+}
+
+pub fn list_pins(chip_name: String) -> AppResult<Vec<GpioPinDescription>> {
+    let chip = Chip::new(format!("/dev/{}", chip_name))?;
+    Ok(chip
+        .lines()
+        .flat_map(|line| match line.info() {
+            Ok(info) => Some(GpioPinDescription::new(info)),
+            Err(e) => {
+                error!("Unable to access line info for line {:?}: {}", line, e);
+                None
+            }
+        })
+        .collect())
+}
+
+#[derive(Serialize)]
+pub struct GpioDeviceDescription {
+    name: String,
+    label: String,
+    num_lines: u32,
+}
+
+impl GpioDeviceDescription {
+    pub fn new(chip: Chip) -> Self {
+        Self {
+            name: chip.name().to_string(),
+            label: chip.label().to_string(),
+            num_lines: chip.num_lines(),
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct GpioPinDescription {
+    name: Option<String>,
+    currently_used_by: Option<String>,
+    is_used: bool,
+    is_kernel: bool,
+    is_output: bool,
+    is_active_low: bool,
+    offset: u32,
+}
+
+impl GpioPinDescription {
+    pub fn new(line: LineInfo) -> Self {
+        Self {
+            name: line.name().map(ToString::to_string),
+            currently_used_by: line.consumer().map(ToString::to_string),
+            is_output: line.direction() == LineDirection::Out,
+            is_used: line.is_used(),
+            is_kernel: line.is_kernel(),
+            is_active_low: line.is_active_low(),
+            offset: line.line().offset(),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,56 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 
 use serde::Serialize;
-use warp::{Filter, Reply};
 use warp::http::StatusCode;
 use warp::reply::{json, with_status};
+use warp::{Filter, Reply};
 
 use application_state::{AppResult, GpioPath, State};
+
+use crate::application_state::{list_chips, list_pins};
 
 mod application_state;
 
 #[tokio::main]
 async fn main() {
-    env_logger::Builder::from_env(
-    env_logger::Env::default().filter_or("LOG", "INFO")
-    ).init();
+    env_logger::Builder::from_env(env_logger::Env::default().filter_or("LOG", "INFO")).init();
 
     let shared_pins_state = Arc::new(State::new());
-    let routes =
-        gpio_get(shared_pins_state.clone())
-            .or(gpio_post(shared_pins_state.clone()))
-            .with(warp::log("http-gpio"));
+    let routes = gpio_list()
+        .or(gpio_pin_list())
+        .or(gpio_get(shared_pins_state.clone()))
+        .or(gpio_post(shared_pins_state.clone()))
+        .with(warp::log("http-gpio"));
 
     let addr: SocketAddr = ([127, 0, 0, 1], 3030).into();
-    warp::serve(routes)
-        .run(addr)
-        .await;
+    warp::serve(routes).run(addr).await;
 }
 
 type StateRef = Arc<State>;
 
-fn gpio_path(shared_pins_state: StateRef) -> impl Filter<Extract=(GpioPath, StateRef,), Error=warp::Rejection> + Clone {
+fn gpio_list() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("gpio")
+        .map(list_chips)
+        .map(create_http_response)
+}
+
+fn gpio_pin_list() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("gpio" / String)
+        .map(list_pins)
+        .map(create_http_response)
+}
+
+fn gpio_path(
+    shared_pins_state: StateRef,
+) -> impl Filter<Extract = (GpioPath, StateRef), Error = warp::Rejection> + Clone {
     warp::path!("gpio" / String / u32)
         .map(GpioPath::new)
         .and(warp::any().map(move || shared_pins_state.clone()))
 }
 
-
-fn gpio_post(state: StateRef) -> impl Filter<Extract=impl warp::Reply, Error=warp::Rejection> + Clone {
+fn gpio_post(
+    state: StateRef,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     gpio_path(state)
         .and(warp::post())
         .and(warp::body::content_length_limit(10))
@@ -46,7 +60,9 @@ fn gpio_post(state: StateRef) -> impl Filter<Extract=impl warp::Reply, Error=war
         .map(create_http_response)
 }
 
-fn gpio_get(state: StateRef) -> impl Filter<Extract=impl warp::Reply, Error=warp::Rejection> + Clone {
+fn gpio_get(
+    state: StateRef,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     gpio_path(state)
         .and(warp::get())
         .map(|gpio_path, state: Arc<State>| state.read(gpio_path))
@@ -54,7 +70,11 @@ fn gpio_get(state: StateRef) -> impl Filter<Extract=impl warp::Reply, Error=warp
 }
 
 fn create_http_response<O: Serialize>(r: AppResult<O>) -> impl warp::Reply {
-    let status = if r.is_err() { StatusCode::INTERNAL_SERVER_ERROR } else { StatusCode::OK };
+    let status = if r.is_err() {
+        StatusCode::INTERNAL_SERVER_ERROR
+    } else {
+        StatusCode::OK
+    };
     let body: Box<dyn Reply> = r
         .map(|o| Box::new(json(&o)) as Box<dyn Reply>)
         .unwrap_or_else(|err| Box::new(err.to_string()));
